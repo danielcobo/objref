21aab0a27c382b2de99f6de14735a75f3aa32869
danielcobo <danielcobo@users.noreply.github.com>
2021-10-29 11:42:57 +0200

    Move to ./src and refactor

---
+++ src/index.js
@@ -0,0 +1,30 @@
+/**
+ * Get deep property of an object via a string path
+ * @public
+ * @param {Object} obj - data object
+ * @param {string} path - reference path
+ * @param {string} [separator='/'] - separator used in path
+ * @param {boolean} buildPath - true/false to define undefined object subpaths
+ * @returns
+ */
+module.exports = function objRef(obj, path, separator, buildPath) {
+  if (typeof separator !== 'string') {
+    separator = '/';
+  }
+  const pathParts = path.split(separator);
+  let ref = obj;
+
+  for (let i = 0; i < pathParts.length; ++i) {
+    const prop = pathParts[i];
+    if (typeof ref[prop] === 'undefined') {
+      if (buildPath) {
+        ref[prop] = {};
+      } else {
+        throw new Error('Invalid path');
+      }
+    }
+    ref = ref[prop];
+  }
+
+  return ref;
+};
---
+++ src/index.test.js
@@ -0,0 +1,30 @@
+const objRef = require('./index.js');
+const a = {
+  b: {
+    c: {
+      d: 1,
+      e: 2,
+    },
+  },
+};
+
+test('objRef() - basic use', function () {
+  const d = objRef(a, 'b/c/d');
+  expect(d).toStrictEqual(1);
+});
+
+test('objRef() - custom separator', function () {
+  const e = objRef(a, 'b-c-e', '-');
+  expect(e).toStrictEqual(2);
+});
+
+test('objRef() - buildPath', function () {
+  const f = objRef(a, 'b/c/f', '/', true);
+  const abc = objRef(a, 'b/c');
+  abc.f = 100;
+  expect(a.b.c.f).toStrictEqual(100);
+});
+
+test('objRef() - invalid path', function () {
+  expect(() => objRef(a, 'b/wrongpath')).toThrow('Invalid path');
+});
